26392fe07d70aca45a72c4f74e31dccd40588a0f
bromix <braincrusher@Deathmachine>
2014-03-01 15:31:46 +0100

    added missing Element (interface) and a project file for tests

--- src/main/java/org/bromix/msbuild/elements/Element.java
+++ src/main/java/org/bromix/msbuild/elements/Element.java
@@ -1,9 +1,16 @@
 package org.bromix.msbuild.elements;
 
 /**
- *
+ * This interface represents the base of each element.
+ * Every element has at least a name (xml-tag). Also we use the interface for
+ * list operations if a class uses children of elements.
+ * 
  * @author Matthias Bromisch
  */
 public interface Element {
+    /**
+     * Returns the name of the element
+     * @return name of the element
+     */
     public String getName();
 }
